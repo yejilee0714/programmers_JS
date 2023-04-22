3426d2f0553e3324da8955ae6e6ae7687d41ded4
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-04-22 13:44:03 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,11 +1,3 @@
 function solution(myString) {
-    var answer = '';
-    for(let i = 0; i< myString.length ; i++){
-        if(myString[i] === myString.toUpperCase()){
-            answer += myString[i];
-        }else{
-            answer += myString[i].toUpperCase();
-        }
-    }
-    return answer;
+    return myString.toUpperCase();
 }
