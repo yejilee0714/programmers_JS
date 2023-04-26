bfffa1275c42927859cc4c7d7339ebe04c355718
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-04-26 17:51:26 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,16 +1,16 @@
-function solution(num_list) {
-    var answer = 0;
-    let add = 0;
-    let mul = 1;
-    num_list.forEach((item)=>{
-        if(num_list.length >= 11){
-            add += item
-            answer = add;
-        }else {
-            mul *= item;
-            answer = mul            
-        }    
-    })
+// function solution(num_list) {
+    // var answer = 0;
+    // let add = 0;
+    // let mul = 1;
+    // num_list.forEach((item)=>{
+    //     if(num_list.length >= 11){
+    //         add += item
+    //         answer = add;
+    //     }else {
+    //         mul *= item;
+    //         answer = mul            
+    //     }    
+    // })
     // for(let i = 0; i < num_list.length ; i++){
     //     if(num_list.length >= 11){
     //         add += num_list[i]
@@ -20,5 +20,24 @@ function solution(num_list) {
     //         answer = mul            
     //     }    
     // }
+    // return answer;
+// }
+
+function solution(num_list) {
+    var answer = 0;
+    let mul = 1;
+    
+    if(num_list.length >= 11){
+        num_list.forEach((a)=>{
+        answer += a   
+       })
+    }
+    else{
+        num_list.forEach((a)=>{
+        mul *=a;
+       })
+       answer = mul   
+    }
+    
     return answer;
 }
