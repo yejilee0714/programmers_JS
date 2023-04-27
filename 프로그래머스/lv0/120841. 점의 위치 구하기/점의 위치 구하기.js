1f44ad53eab3da8dbcbba985aea068aa8339d54e
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-04-27 16:49:36 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,10 +1,19 @@
 function solution(dot) {
-    let result = 0;
+    let first = dot[0]
+        let second = dot[1]
+        if(0 < first && 0 < second){
+            return 1
+        } else if (0 > first && 0 < second){
+            return 2
+        } else if (0 > first && 0 > second){
+            return 3
+        } else return 4
+//     let result = 0;
     
-    if(dot[0] > 0){
-        (dot[1] > 0) ? result = 1 : result = 4;  
-    } else{
-        (dot[1] > 0) ? result = 2 : result = 3;
-    }
-    return result;
+//     if(dot[0] > 0){
+//         (dot[1] > 0) ? result = 1 : result = 4;  
+//     } else{
+//         (dot[1] > 0) ? result = 2 : result = 3;
+//     }
+//     return result;
 }
