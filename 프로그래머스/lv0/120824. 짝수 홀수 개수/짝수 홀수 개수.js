3fb6279918b36cf9ff90289e9ed65dc3f3acc1c7
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-01 17:29:15 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -2,9 +2,13 @@ function solution(num_list) {
     var answer = [];
     let a = 0;
     let b = 0;
-    for(let i = 0; i < num_list.length ; i++){
-        (num_list[i] % 2 == 0)? a += 1 : b += 1;
-        answer=[a , b];
-    }
+    // for(let i = 0; i < num_list.length ; i++){
+    //     (num_list[i] % 2 == 0)? a += 1 : b += 1;
+    //     answer=[a , b];
+    // }
+    num_list.forEach((item)=>{
+        (item % 2 == 0)? a+=1 : b+=1;
+        answer = [a, b];
+    })
     return answer;
 }
