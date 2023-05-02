c0eedac0b007fab47b5ee5974200c9f2bccd86fd
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-02 16:52:46 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 6.06 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -1,13 +1,21 @@
+// function solution(arr) {
+//     var answer = [];
+//     [...arr].forEach((item)=>{
+//         if(item<50 && item % 2 !==0){
+//             answer.push(item*2)
+//         }else if(item>= 50 && item % 2 === 0){
+//             answer.push(item/2)
+//         }else{
+//             answer.push(item)
+//         }
+//     })
+//     return answer;
+// }
+
 function solution(arr) {
-    var answer = [];
-    [...arr].forEach((item)=>{
-        if(item<50 && item % 2 !==0){
-            answer.push(item*2)
-        }else if(item>= 50 && item % 2 === 0){
-            answer.push(item/2)
-        }else{
-            answer.push(item)
-        }
-    })
+    let answer = [];
+    for (let i = 0; i<arr.length; i++){
+        (arr[i]>=50 && arr[i] % 2===0) ? answer.push(arr[i]/2) : (arr[i]<50 && arr[i] % 2!==0) ? answer.push(arr[i]*2) : answer.push(arr[i])
+    }
     return answer;
 }
