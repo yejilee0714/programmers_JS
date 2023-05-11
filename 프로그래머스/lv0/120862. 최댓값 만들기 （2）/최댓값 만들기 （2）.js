7f05b6864582b594eca07184740cbf542d68a2ae
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-11 16:42:11 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    numbers.sort((a, b)=> a-b)
+    let num1 = numbers[0] * numbers[1];
+    let num2 = numbers[numbers.length-1] *numbers[numbers.length-2];        
+    return (num1 > num2) ? num1 : num2;
+}
