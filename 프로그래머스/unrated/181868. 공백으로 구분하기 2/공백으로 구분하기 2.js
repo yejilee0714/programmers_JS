510bfcf3e9d9a5671f0c1a678365b576dfb5bfaf
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-11 16:14:44 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,10 +1,10 @@
 function solution(my_string) {
-    var answer = [];
-    let new_list = my_string.split(" ");
-    new_list.forEach((item)=>{
-        if(item !==""){
-            answer.push(item)
-        }
-    })
-    return answer;
+    // var answer = [];
+    // let new_list = my_string.split(" ");
+    // new_list.forEach((item)=>{
+    //     if(item !==""){
+    //         answer.push(item)
+    //     }
+    // })
+    return my_string.split(" ").filter(item=> item !== "");
 }
