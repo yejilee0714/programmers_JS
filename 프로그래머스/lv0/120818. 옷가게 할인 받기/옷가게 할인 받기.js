f42086699ea150a46cba48f16dbf1d7a9054762c
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-12 17:08:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,3 +1,3 @@
 function solution(price) {
-    return (price >= 500000) ? parseInt(price * 0.8) : (price >= 300000) ? parseInt(price * 0.9) : (price >= 100000) ? parseInt(price * 0.95) : parseInt(price);
+    return (price >= 500000) ? price * 0.8 : (price >= 300000) ? price * 0.9 : (price >= 100000) ? parseInt(price * 0.95) : price;
 }
