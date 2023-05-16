17df69fa14c17bec61d71c45addc3e153080795d
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-16 16:18:06 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,14 +1,4 @@
 function solution(array) {
-    var answer = [];
-    let max = 0;
-    let index = 0;
-    for(let i=0; i< array.length; i++){
-        if( max < array[i]){
-            max = array[i];
-            index = i;
-        }
-    }
-    answer[0] = max;
-    answer[1] = index;
-    return answer;
+    let max = Math.max(...array);
+    return [max, array.indexOf(max)];
 }
