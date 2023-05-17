0d7c3b6bd8dea935b478e44d025a584127af12a7
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-17 17:15:48 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,3 +1,3 @@
 function solution(hp) {
-    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + ((hp % 5) % 3);
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor((hp % 5) % 3);
 }
