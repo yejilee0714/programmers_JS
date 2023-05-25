888c991d11b12fbab587dfddd25dac5d618cff2f
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-25 17:25:30 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,8 +1,3 @@
 function solution(order) {
-  var answer = 0;
-  order.toString().split('').map((v) => {
-    if (parseInt(v) !== 0 && parseInt(v) % 3 === 0) 
-      answer += 1;
-  });
-  return answer;
+    return [...String(order)].filter(item=> ["3", "6", "9"].includes(item)).length;
 }
