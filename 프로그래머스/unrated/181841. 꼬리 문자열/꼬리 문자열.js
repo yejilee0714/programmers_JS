652f896a26f065460676763d0b5e2cc1986f9934
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-05-28 22:50:27 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,9 @@
+function solution(str_list, ex) {
+    var answer = '';
+    for(let i = 0; i < str_list.length ; i++){
+       if(str_list[i].indexOf(ex) === -1) {
+           answer += str_list[i];
+        }
+    }
+    return answer;
+}
