bef72032a1a8a05973779fc0e05514e4b63d2eb8
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-07-06 23:16:51 +0900

    [unrated] Title: 수 조작하기 2, Time: 7.32 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,15 @@
+function solution(numLog) {
+    var answer = '';
+    for(let i = 0 ; i < numLog.length ; i++){
+        if(numLog[i+1] === numLog[i] + 1){
+            answer += 'w'
+        } else if(numLog[i+1] === numLog[i] - 1){
+            answer += 's'
+        } else if(numLog[i+1] === numLog[i] + 10){
+            answer += 'd'
+        } else if(numLog[i+1] === numLog[i] - 10){
+            answer += 'a'
+        }
+    }
+    return answer;
+}
