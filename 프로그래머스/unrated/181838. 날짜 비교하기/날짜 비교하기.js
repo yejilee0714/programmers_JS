7fa3ebb0864c83ac7b73940e4ebe9ad1a68589ba
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-07-13 14:25:22 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,5 @@
+function solution(date1, date2) {
+    // var answer = 0;
+    return (date1[0] < date2[0]) ? 1 : (date1[0] > date2[0]) ? 0 : (date1[1] < date2[1]) ? 1 : (date1[1] > date2[1]) ? 0 : (date1[2] < date2[2]) ? 1 : 0;
+    // return answer;
+}
