6b03ce4b7eda258b91dd1c2d27d554dce2b6a7a7
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2023-08-13 23:56:45 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,11 +1,3 @@
 function solution(a, b) {
-    var answer = 0;
-    if(a % 2 !== 0 && b % 2 !==0){
-        answer = (a**2) + (b**2);
-    }else if(a % 2 !==0 || b % 2 !==0){
-        answer = 2 * (a+b);
-    }else{
-        answer = Math.abs(a - b);
-    }
-    return answer;
+    return (a % 2 !== 0 && b % 2 !==0) ? (a**2) + (b**2) : (a % 2 !==0 || b % 2 !==0) ? 2 * (a+b) : Math.abs(a - b);
 }
