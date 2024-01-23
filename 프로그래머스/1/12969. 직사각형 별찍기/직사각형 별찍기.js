205dbe233408bd74d39c6791f04bf97d6b84916f
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2024-01-24 00:22:22 +0900

    [level 1] Title: 직사각형 별찍기, Time: 55.29 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -3,12 +3,12 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
     for(let i = 0 ; i < b ; i++){
-        let star = "";
+//         let star = "";
         
-        for(let j = 0; j < a; j++){
-            star += "*";
-        }
+//         for(let j = 0; j < a; j++){
+//             star += "*";
+//         }
         
-        console.log(star);
+        console.log('*'.repeat(a));
     }
 });
