faa66fca3e56f0467948d4edd6fb4b72ff3d9575
yejilee0714 <121578822+yejilee0714@users.noreply.github.com>
2024-02-01 22:36:06 +0900

    [level 0] Title: 배열 만들기 6, Time: 6.51 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+    var answer = [arr[0]];
+    for( let i = 1; i < arr.length ; i++) { 
+        answer[answer.length - 1] === arr[i] ? answer.pop() : answer.push(arr[i]);
+    }
+    return answer.length ? answer : [-1];
+}
